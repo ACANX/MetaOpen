721b89735afde4a90d5731854a8829359806de9e
ACANX <245818784@qq.com>
2025-10-01 03:52:03 +0800

    update：添加bean的get、set方法

--- meta-model/model-quote/src/main/java/com/acanx/meta/model/trans/TransSecuMin.java
+++ meta-model/model-quote/src/main/java/com/acanx/meta/model/trans/TransSecuMin.java
@@ -87,4 +87,92 @@ public class TransSecuMin {
                 ", e='" + e + '\'' +
                 '}';
     }
+
+    public Long getTs() {
+        return ts;
+    }
+
+    public void setTs(Long ts) {
+        this.ts = ts;
+    }
+
+    public String getM() {
+        return m;
+    }
+
+    public void setM(String m) {
+        this.m = m;
+    }
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+
+    public BigDecimal getO() {
+        return o;
+    }
+
+    public void setO(BigDecimal o) {
+        this.o = o;
+    }
+
+    public BigDecimal getC() {
+        return c;
+    }
+
+    public void setC(BigDecimal c) {
+        this.c = c;
+    }
+
+    public BigDecimal getH() {
+        return h;
+    }
+
+    public void setH(BigDecimal h) {
+        this.h = h;
+    }
+
+    public BigDecimal getL() {
+        return l;
+    }
+
+    public void setL(BigDecimal l) {
+        this.l = l;
+    }
+
+    public BigDecimal getAd() {
+        return ad;
+    }
+
+    public void setAd(BigDecimal ad) {
+        this.ad = ad;
+    }
+
+    public BigDecimal getV() {
+        return v;
+    }
+
+    public void setV(BigDecimal v) {
+        this.v = v;
+    }
+
+    public BigDecimal getT() {
+        return t;
+    }
+
+    public void setT(BigDecimal t) {
+        this.t = t;
+    }
+
+    public String getE() {
+        return e;
+    }
+
+    public void setE(String e) {
+        this.e = e;
+    }
 }
