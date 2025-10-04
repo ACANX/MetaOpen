1ea77b0c185245454d7293b2b30c5d40952444ea
ACANX <245818784@qq.com>
2025-10-05 06:20:56 +0800

    feat:添加rss相关的对象

---
+++ meta-model/model-rss/src/main/java/com/acanx/meta/model/json/feed/JSONFeedConst.java
@@ -0,0 +1,16 @@
+package com.acanx.meta.model.json.feed;
+
+/**
+ * JSONFeedConst
+ *
+ * @author ACANX
+ * @since 20250926
+ */
+public class JSONFeedConst {
+
+    public static final String VERSION = "version";
+
+    public static final String URL_V_1_1 = "https://jsonfeed.org/version/1.1";
+
+
+}
---
+++ meta-model/model-rss/src/main/java/com/acanx/meta/model/rss/Channel.java
@@ -0,0 +1,12 @@
+package com.acanx.meta.model.rss;
+
+/**
+ * Channel
+ *
+ * @author ACANX
+ * @since 20251005
+ */
+public class Channel {
+
+
+}
