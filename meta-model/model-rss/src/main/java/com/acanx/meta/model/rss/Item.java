e4ee0cbbfb4d5f5c9d33ebe910a682fb6dc0bab1
ACANX <acanx@qq.com>
2025-11-09 06:16:30 +0800

    fix:去除lombok依赖，增加getter、setter方法 (#550)

--- meta-model/model-rss/src/main/java/com/acanx/meta/model/rss/Item.java
+++ meta-model/model-rss/src/main/java/com/acanx/meta/model/rss/Item.java
@@ -1,14 +1,11 @@
 package com.acanx.meta.model.rss;
 
-import lombok.Data;
-
 /**
  * Item
  *
  * @author ACANX
  * @since 20251004
  */
-@Data
 public class Item {
 
     private String title;
@@ -38,4 +35,117 @@ public class Item {
     private String keywords;
 
     private String urlImage;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPubDate() {
+        return pubDate;
+    }
+
+    public void setPubDate(String pubDate) {
+        this.pubDate = pubDate;
+    }
+
+    public String getPubDateTime() {
+        return pubDateTime;
+    }
+
+    public void setPubDateTime(String pubDateTime) {
+        this.pubDateTime = pubDateTime;
+    }
+
+    public String getGuid() {
+        return guid;
+    }
+
+    public void setGuid(String guid) {
+        this.guid = guid;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public String getEnclosure() {
+        return enclosure;
+    }
+
+    public void setEnclosure(String enclosure) {
+        this.enclosure = enclosure;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
 }
