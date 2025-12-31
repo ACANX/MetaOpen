2265bad3a4e2f80205caaadbddcde91bf9ff21e8
ACANX <245818784@qq.com>
2025-12-31 20:46:12 +0800

    feat: add new module model-folo

---
+++ meta-model/model-folo/src/main/java/com/acanx/meta/model/folo/App.java
@@ -0,0 +1,12 @@
+package com.acanx.meta.model.folo;
+
+/**
+ *
+ * App
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
