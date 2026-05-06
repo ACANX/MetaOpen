b95fc28955c502cda7757c2b228e485c3ce8cacb
ACANX <245818784@qq.com>
2026-05-07 01:32:42 +0800

    feat: 添加模块base-rest

---
+++ base/base-rest/src/main/java/com/acanx/meta/base/rest/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Meta-Base-Rest 包
+ *
+ * 提供基础REST API组件，包括：
+ * - 统一响应结构 (Result/Response)
+ * - 全局异常处理
+ * - 通用分页封装
+ * - REST工具类
+ *
+ * @author ACANX
+ * @since 0.1.5
+ */
+package com.acanx.meta.base.rest;
