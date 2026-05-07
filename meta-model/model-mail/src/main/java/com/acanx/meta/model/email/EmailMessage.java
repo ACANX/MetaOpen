d44e6442dc704565300b51de33188c9e9132c4ee
ACANX <245818784@qq.com>
2026-05-08 03:00:23 +0800

    feat: 添加模块model-mail

---
+++ meta-model/model-mail/src/main/java/com/acanx/meta/model/email/EmailMessage.java
@@ -0,0 +1,34 @@
+package com.acanx.meta.model.email;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * EmailMessage
+ *
+ * @author ACANX
+ * @since 20260507
+ */
+//@Builder
+@Data
+public class EmailMessage {
+
+    private EmailSender sender;
+
+    private List<EmailAddress> receivers;
+
+//    private String name;
+
+    private String subject;
+
+    private List<String> tags;
+
+    private String htmlContent;
+
+    private List<String> attachments;
+
+    public EmailMessage() {
+    }
+}
